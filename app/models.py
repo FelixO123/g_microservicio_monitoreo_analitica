b2333c0dd65b1aa5560982b007f2825ecb87b941
FelixO123 <fe.omenaca@duocuc.cl>
2026-04-26 01:19:32 -0400

    FUNCION KPI LISTA

--- app/models.py
+++ app/models.py
@@ -11,19 +11,3 @@ class KPI(Base):
     # Usamos timezone.utc para evitar problemas de horario
     fecha_calculo = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
-class Reporte(Base):
-    __tablename__ = "reportes"
-    id_reporte = Column(Integer, primary_key=True, index=True)
-    fecha_generacion = Column(DateTime, default=lambda: datetime.now(timezone.utc))
-    resumen = Column(String)
-    estado_general = Column(String)
-
-class MetricaProyecto(Base):
-    __tablename__ = "metricas_proyecto"
-    id_metrica = Column(Integer, primary_key=True, index=True)
-    # Esta es tu FK Lógica. No lleva ForeignKey("proyectos.id") 
-    # porque la tabla proyectos NO existe en esta DB.
-    id_proyecto = Column(Integer, nullable=False, index=True) 
-    porcentaje_avance = Column(Float, default=0.0)
-    tareas_completadas = Column(Integer, default=0)
-    tareas_totales = Column(Integer, default=0)
